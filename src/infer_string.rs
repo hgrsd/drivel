daefbb67a45a7ce97959bc48eaae100e64b7c8a2
Daniël <40741030+hgrsd@users.noreply.github.com>
2024-07-04 15:51:33 +0100

    perf: Optimise string inference (#14)
    * perf: string inference runs fewer expensive tests
    
    * make matching nicer to work with

--- src/infer_string.rs
+++ src/infer_string.rs
@@ -11,27 +11,62 @@ lazy_static! {
         regex::Regex::new(r"[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$").unwrap();
 }
 
-pub(crate) fn infer_string_type(s: &str) -> StringType {
-    if ISO_DATE_REGEX.is_match(s) {
-        StringType::IsoDate
-    } else if chrono::DateTime::parse_from_rfc2822(s).is_ok() {
-        StringType::DateTimeISO8601
-    } else if chrono::DateTime::parse_from_rfc3339(s).is_ok() {
-        StringType::DateTimeISO8601
-    } else if UUIDREGEX.is_match(s) {
-        StringType::UUID
-    } else if EMAIL_REGEX.is_match(s) {
-        StringType::Email
-    } else if url::Url::parse(s).is_ok() {
-        StringType::Url
-    } else if HOSTNAME_REGEX.is_match(s) {
-        StringType::Hostname
+fn uuid(s: &str) -> Option<StringType> {
+    if s.len() == 36 && UUIDREGEX.is_match(s) {
+        Some(StringType::UUID)
     } else {
-        StringType::Unknown {
-            strings_seen: vec![s.to_owned()],
-            chars_seen: s.chars().collect(),
-            min_length: Some(s.len()),
-            max_length: Some(s.len()),
+        None
+    }
+}
+
+fn email(s: &str) -> Option<StringType> {
+    if s.contains('@') && EMAIL_REGEX.is_match(s) {
+        Some(StringType::Email)
+    } else {
+        None
+    }
+}
+
+fn url_host(s: &str) -> Option<StringType> {
+    if s.contains('.') {
+        if url::Url::parse(s).is_ok() {
+            return Some(StringType::Url);
+        }
+        if HOSTNAME_REGEX.is_match(s) {
+            return Some(StringType::Hostname);
         }
     }
+    None
+}
+
+fn dates(s: &str) -> Option<StringType> {
+    if s.chars().take(1).all(|char| char.is_numeric()) {
+        if ISO_DATE_REGEX.is_match(s) {
+            return Some(StringType::IsoDate);
+        }
+        if chrono::DateTime::parse_from_rfc3339(s).is_ok() {
+            return Some(StringType::DateTimeISO8601);
+        }
+    }
+
+    if chrono::DateTime::parse_from_rfc2822(s).is_ok() {
+        return Some(StringType::DateTimeISO8601);
+    }
+
+    None
+}
+
+pub(crate) fn infer_string_type(s: &str) -> StringType {
+    for matcher in [uuid, email, url_host, dates] {
+        if let Some(string_type) = matcher(s) {
+            return string_type;
+        }
+    }
+
+    return StringType::Unknown {
+        strings_seen: vec![s.to_owned()],
+        chars_seen: s.chars().collect(),
+        min_length: Some(s.len()),
+        max_length: Some(s.len()),
+    };
 }
